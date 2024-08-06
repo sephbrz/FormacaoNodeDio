84e8b2ed266c25e82dea7878f13381f7dbe78538
sephbrz <103461170+sephbrz@users.noreply.github.com>
2024-08-06 16:50:52 -0300

    Versao inicial semelhante a aula, com os players hardcoded
    Versao inicial semelhante a aula, com os players hardcoded

---
+++ 03-mario_kart_console/src/index.js
@@ -0,0 +1,56 @@
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+  };
+  
+  const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+  };
+    
+  const player3 = {
+    NOME: "Peach",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 2,
+    PONTOS: 0,
+  };
+  
+  const player4 = {
+    NOME: "Yoshi",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 4,
+    PODER: 3,
+    PONTOS: 0,
+  };
+ 
+  const player5 = {
+    NOME: "Donkey Kong",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  };
+  
+  const player6 = {
+    NOME: "Bowser",
+    VELOCIDADE: 5,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  };
+
+
+async function rollDice(){
+    return Math.floor(Math.random() * 6) + 1
+};
+
+(async function main(){
+    console.log("teste");
+})();
